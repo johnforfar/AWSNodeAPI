258177042778bf09a41f2b1562ca32d7feb9a7ad
johnforfar <john.forfar@gmail.com>
2023-05-07 14:39:11 +1000

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ const port = 8080;
 var myUsers = ['Tom', 'Nick', 'Harry'];
 
 app.get("/", (req, res) => {
-    res.write("########################################################\n");
+    res.write("#########################################################\n");
     res.write("Welcome to the AWS Node API!\n");
-    res.write("########################################################\n");
+    res.write("#########################################################\n");
     res.write("The following endpoints have been configured:\n");
     res.write("GET /listUsers\n");
     res.write("PUT /addUser?name=John\n");
     res.write("DELETE /deleteUser?start=0&deleteCount=1\n");
     res.write("POST /updateUser?index=0&name=John\n");
-    res.write("########################################################\n");
+    res.write("#########################################################\n");
     res.write("Built with Node.js and Express.js\n")
     res.write("Deployed with AWS CodePipeline and AWS Elastic Beanstalk.\n");
-    res.write("########################################################\n");
+    res.write("#########################################################\n");
     res.write("Check out johnforfar.com for more portfolio projects.\n");
-    res.write("########################################################\n");
+    res.write("#########################################################\n");
     res.end()
 })
 
